4250ddc9966e189e733b5af68f0f296fbcb8e81f
Sefleris <p.sefleris@gmail.com>
2025-02-05 10:12:50 +0200

    Reorder python project file structure

--- js-classwork/241213-firebase-training-js24-main/src/firebase.js
+++ js-classwork/241213-firebase-training-js24-main/src/firebase.js
@@ -7,12 +7,12 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2rSxCwg-_Crgli7oqcE1BF-EmmUmzl4w",
-  authDomain: "quick-test-78f7c.firebaseapp.com",
-  projectId: "quick-test-78f7c",
-  storageBucket: "quick-test-78f7c.firebasestorage.app",
-  messagingSenderId: "733318848777",
-  appId: "1:733318848777:web:d2737d470a6e45e09433d8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
